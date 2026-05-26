811cea823788975b9816fa3e39b6db4b294c22b4
RomanBalabaev <roman.balabaev@via-mobi.com>
2026-05-27 02:08:55 +0300

    modify types

--- src/entities/wheel/model/types.ts
+++ src/entities/wheel/model/types.ts
@@ -2,9 +2,11 @@ export interface IWheel {
     wheel_id: number;
     owner_id: number;
     name: string;
-    interval_seconds: number;
+    interval_seconds?: number;
     fields: IField[];
 }
+
+export type IWheelSave = Omit<IWheel, 'wheel_id' | 'owner_id'> & Partial<IWheel>
 
 export interface IField {
     field_id: number;
