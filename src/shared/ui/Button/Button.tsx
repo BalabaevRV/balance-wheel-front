657fc8a24fb4744c53ab8716610726f81f624c68
RomanBalabaev <roman.balabaev@via-mobi.com>
2026-05-29 03:23:51 +0300

    add additional class to button component

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,13 +1,14 @@
 interface IButtonProps {
-  children: React.ReactNode;
-  onClick?: () => void;
-  type?: 'button' | 'submit' | 'reset';
-  variant?: 'primary' | 'secondary';
-  disabled?: boolean;
+    children: React.ReactNode;
+    onClick?: () => void;
+    type?: 'button' | 'submit' | 'reset';
+    variant?: 'primary' | 'secondary';
+    disabled?: boolean;
+    className?: string;
 }
 
-export const Button = ({ children, onClick, type = 'button' }: IButtonProps) => {
-    return <button type={type} className="bg-blue-800 p-2 text-white rounded-lg cursor-pointer hover:bg-blue-700 active:bg-sky-900 transition-colors" onClick={onClick}>
+export const Button = ({ children, onClick, type = 'button', className = '' }: IButtonProps) => {
+    return <button type={type} className={`bg-blue-800 p-2 text-white rounded-lg cursor-pointer hover:bg-blue-700 active:bg-sky-900 transition-colors ${className}`} onClick={onClick}>
         { children }
     </button>
 }
